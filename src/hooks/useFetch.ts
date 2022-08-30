2d57e641e5c85b0f9432a7bb5dd3ce1522b69304
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-08-30 22:38:00 +0200

    added custom fetch hook 	new file:   src/hooks/useFetch.ts

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+type Api = {
+  category: number;
+  questions: number;
+};
+
+type QuestionsProps = {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: [string];
+};
+
+const useFetch = (api: Api): [QuestionsProps[], boolean] => {
+  const [question, setQuestions] = useState<QuestionsProps[]>([]);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=${api.questions}&category=${api.category}`,
+    );
+
+    const data = await response.json();
+    if (data.response_code === 0) {
+      setQuestions(data.results);
+      console.log(question);
+    } else {
+      throw new Error("no data returned");
+    }
+  };
+  useEffect(() => {
+    fetchData().catch(() => setError(true));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [api]);
+  return [question, error];
+};
+
+export default useFetch;
