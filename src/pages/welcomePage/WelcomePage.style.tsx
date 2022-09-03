db17e5730b592bf43ecd9c62b9899e5102185f42
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-03 18:25:30 +0200

    changed folder structure 	renamed:    src/pages/AboutPage.tsx -> src/pages/aboutPage/AboutPage.tsx 	renamed:    src/pages/Error404Page.tsx -> src/pages/error404Page/Error404Page.tsx 	renamed:    src/pages/GamePage.tsx -> src/pages/gamePage/GamePage.tsx 	new file:   src/pages/welcomePage/WelcomePage.style.tsx 	renamed:    src/pages/WelcomePage.tsx -> src/pages/welcomePage/WelcomePage.tsx 	renamed:    src/pages/WelcomePage.types.tsx -> src/pages/welcomePage/WelcomePage.types.tsx

---
+++ src/pages/welcomePage/WelcomePage.style.tsx
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: ${({ theme: { primary } }) => primary};
+  &:hover {
+    background-color: ${({ theme: { secondary } }) => secondary};
+  }
+  &:active {
+    background-color: ${({ theme: { primary } }) => primary};
+  }
+`;
+export const Input = styled.input`
+  -webkit-appearance: none;
+  margin-right: 15px;
+  width: 300px;
+  height: 8px;
+  border-radius: 5px;
+  background-image: linear-gradient(
+    ${({ theme: { primary } }) => primary},
+    ${({ theme: { primary } }) => primary}
+  );
+  background-color: ${({ theme: { inputBackground } }) => inputBackground};
+  background-repeat: no-repeat;
+  background-size: 0% 100%;
+  position: relative;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: ${({ theme: { primary } }) => primary};
+    cursor: ew-resize;
+    box-shadow: 0 0 0px 1px ${({ theme: { inputBoxShadow } }) => inputBoxShadow};
+    transition: box-shadow 0.3s ease-in-out;
+    position: relative;
+    &:hover {
+      box-shadow: 0px 0px 0px 15px ${({ theme: { inputBoxShadow } }) => inputBoxShadow};
+    }
+  }
+  &::-webkit-slider-thumb:active {
+    box-shadow: 0px 0px 0px 15px ${({ theme: { inputBoxShadowActive } }) => inputBoxShadowActive};
+  }
+  &::-webkit-slider-runnable-track {
+    -webkit-appearance: none;
+    box-shadow: none;
+    border: none;
+    background: transparent;
+  }
+  //special firefox treatment...
+  &::-moz-range-thumb {
+    height: 20px;
+    width: 20px;
+    border: none;
+    border-radius: 50%;
+    background-color: ${({ theme: { primary } }) => primary};
+    cursor: ew-resize;
+  }
+`;
--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -2,87 +2,11 @@ import navlogo from "@assets/images/navlogo.webp";
 import GridCategory from "@components/gridCategory/GridCategory";
 import { GridCategories } from "@components/gridCategory/GridCategory.style";
 import PageWrapper from "@components/pageWrapper/PageWrapper";
-import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
+import { Button, Input } from "./WelcomePage.style";
 import type WelcomePageProps from "./WelcomePage.types";
 
-const Button = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 5px;
-  border-radius: 5px;
-  background-color: #4527a0;
-  &:hover {
-    background-color: #7953d2;
-  }
-  &:active {
-    background-color: #4527a0;
-  }
-`;
-const Input = styled.input`
-  ::-moz-range-track {
-    display: none;
-  }
-  ::-moz-range-thumb {
-    display: none;
-  }
-  -webkit-appearance: none;
-  margin-right: 15px;
-  width: 300px;
-  height: 8px;
-  border-radius: 5px;
-  background-image: linear-gradient(#4527a0, #4527a0);
-  background-color: rgba(69, 39, 160, 0.38);
-  background-repeat: no-repeat;
-  background-size: 0% 100%;
-  position: relative;
-  &:focus {
-    outline: none;
-  }
-  :before {
-    width: 20px;
-    height: 20px;
-    background-color: white;
-    top: 0;
-    left: 0;
-  }
-
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    height: 20px;
-    width: 20px;
-    border-radius: 50%;
-    background: #4527a0;
-    cursor: ew-resize;
-    box-shadow: 0 0 0px 1px rgba(69, 39, 160, 0.2);
-    transition: background 0.3s ease-in-out;
-    transition: box-shadow 0.3s ease-in-out;
-    position: relative;
-  }
-  &::-webkit-slider-thumb:hover {
-    box-shadow: 0px 0px 0px 15px rgba(69, 39, 160, 0.2);
-    position: relative;
-    &:before {
-      width: 50px;
-      height: 20px;
-      top: -20px;
-      left: 0;
-      position: absolute;
-      background-color: white;
-    }
-  }
-  &::-webkit-slider-thumb:active {
-    box-shadow: 0px 0px 0px 15px rgba(69, 39, 160, 0.4);
-  }
-  &::-webkit-slider-runnable-track {
-    -webkit-appearance: none;
-    box-shadow: none;
-    border: none;
-    background: transparent;
-  }
-`;
 const WelcomePage = ({ setApi, api }: WelcomePageProps) => {
   const categories = [
     { name: "Animals", value: 27 },
@@ -123,7 +47,7 @@ const WelcomePage = ({ setApi, api }: WelcomePageProps) => {
             handleQuestionsNumberOnInput(+e.target.value, e.target)
           }
         />
-        Questions
+        Questions {api.questions}
       </label>
       <GridCategories>
         {categories.map((category, i) => (
