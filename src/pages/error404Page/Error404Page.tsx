a2aa4aad1923ea749256924322a4a06a95eb756e
AnotherOtherJ <kuba2323kuba@gmail.com>
2022-09-08 19:56:22 +0200

    Deleted Error Page 	deleted:    src/pages/error404Page/Error404Page.tsx

--- src/pages/error404Page/Error404Page.tsx
+++
@@ -1,8 +0,0 @@
-import PageWrapper from "@components/pageWrapper/PageWrapper";
-import { memo } from "react";
-
-type Props = { something?: boolean };
-const Error404Page = (props: Props) => {
-  return <PageWrapper>Error404Page</PageWrapper>;
-};
-export default memo(Error404Page);
